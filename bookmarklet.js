ef218add678b98007bc604c995d0b41345dd4bf0
server2003user <84295009+Server2003User@users.noreply.github.com>
2024-04-09 17:06:29 -0400

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:void%20function(){let%20a,b=parseInt(document.getElementById(%22pracTotal%22).innerHTML),c=1;for(;b%3Ec;)a=pracContent[pracSide2][pracWord].toLowerCase(),console.log(a),document.getElementById(%22pracBox%22).value=a,checkWord(),nextWord(),c++;a=pracContent[pracSide2][pracWord].toLowerCase(),document.getElementById(%22pracBox%22).value=a,checkWord()}();
